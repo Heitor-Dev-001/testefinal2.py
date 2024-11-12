7ecf6466053e880e618def094b06da3786f8e6a3
Heitor Silva <heitorsilvanascimento@hotmail.com>
2024-11-12 14:23:01 -0300

    Add files via upload

---
+++ testefinal2.py
@@ -0,0 +1,14 @@
+#Qual é o resultado calculado pelo Python para as expressões simples 19 // 2 e 19%2, respectivamente?
+
+#Entrada do usário
+calculo_1 = ('Digite sua conta:')
+
+#Calculo 1
+calculo_1 = 19 // 2
+print('O resultado da sua conta é?:{}'.format(calculo_1))
+print('-=-' * 20)
+
+#Calculo 2
+calculo_1 = 19 % 2
+print('O resultado da sua conta é?:{}'.format(calculo_1))
+print('-=-' * 20)
